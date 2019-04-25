eaef15615fb81c9ab0227736d4f24ade1e555965
mmq981015 <mmq981015@136.com>
2019-04-26 00:26:10 +0800

    add crawl_sale_info

--- qmusic.py
+++ qmusic.py
@@ -5,7 +5,7 @@ QQ音乐粉丝工会数据
 import json
 import requests
 import time
-from sqlalchemy import Column, String, create_engine, Integer, ForeignKey
+from sqlalchemy import Column, String, create_engine, Integer, BigInteger
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import sys
@@ -20,14 +20,30 @@ headers = {
     'Referer': 'https://y.qq.com/m/digitalbum/gold/index.html?mid=000etWPH2Sk6kG&g_f=share_music&_video=true&mod=index&skin=1'
 }
 pg_url = 'postgres://postgres:postgres@localhost:5432/cosmo'
-g_tk = "5381"
-qq = "10031199"  # 随便填
+g_tk = "5381"  # 非登录都是5381
+qq = "10031199"  # 随便填个qq
 sleep_interval = 0  # 请求间隙
 
 engine = create_engine(pg_url)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 Base = declarative_base()
+
+
+class Act(Base):
+    """
+    销售信息
+    """
+    __tablename__ = 'qm_act'
+    mid = Column(String(32))
+    actid = Column(Integer, primary_key=True)  # 动作id？
+    publictime = Column(String(32))
+    price = Column(Integer)
+    soldamt = Column(BigInteger)
+    sold_album_cnt = Column(BigInteger)
+    album_name = Column(String(32))
+    singername = Column(String(32))
+    songcnt = Column(Integer)
 
 
 class Union(Base):
@@ -40,6 +56,7 @@ class Union(Base):
     unionname = Column(String(128))  # 工会名
     iscore = Column(Integer)  # 销量
     cnt = Column(Integer)  # 账户数
+    a_actid = Column(Integer)  # 动作id
 
 
 class Acct(Base):
@@ -47,14 +64,38 @@ class Acct(Base):
     账户
     """
     __tablename__ = 'qm_acct'
+    id = Column(Integer, primary_key=True)
     irank = Column(Integer)  # 销量排名
-    uuin = Column(String(128), primary_key=True)  # qq号一览无余
+    uuin = Column(String(128))  # qq号一览无余
     strnick = Column(String(128))  # 昵称
     iscore = Column(Integer)  # 销量
     u_uuin = Column(Integer)  # 工会id
+    a_actid = Column(Integer)  # 动作id
 
 
-def crawl_union(begin=0, end=9, autoPage=True):
+def crawl_sale_info(albummid):
+    url = "https://c.y.qq.com/v8/fcg-bin/musicmall.fcg?" \
+          "g_tk={}&uin={}&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&albumid=&albummid={}&cmd=get_base_sale_info&songlist=1&desc=1&singerinfo=1&salecount=1&_={}" \
+        .format(g_tk, qq, albummid, int(time.time()))
+    req = requests.get(url, headers=headers)
+    js = json.loads(req.text)
+    actid = js['data']['actid']
+    publictime = js['data']['publictime']
+    price = js['data']['price']
+    soldamt = js['data']['soldamt']
+    sold_album_cnt = js['data']['sold_album_cnt']
+    album_name = js['data']['album_name']
+    singername = js['data']['singerinfo'][0]['singername']
+    songcnt = len(js['data']['songlist'])
+    item = Act(mid=albummid, actid=actid, publictime=publictime, price=price, soldamt=soldamt,
+               sold_album_cnt=sold_album_cnt,
+               album_name=album_name, singername=singername, songcnt=songcnt)
+    session.add(item)
+    session.commit()
+    return actid
+
+
+def crawl_union(actid, begin=0, end=9, autoPage=True):
     """
     爬取工会信息
     :param begin:
@@ -62,7 +103,7 @@ def crawl_union(begin=0, end=9, autoPage=True):
     :param autoPage:
     :return:
     """
-    rankname = "group_rank_peract_339"
+    rankname = "group_rank_peract_{}".format(actid)
     while True:
         gh_url = "https://c.y.qq.com/shop/fcgi-bin/fcg_album_rank?" \
                  "g_tk={}&uin={}&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&begin={}&end={}&rankname={}&_={}" \
@@ -71,9 +112,9 @@ def crawl_union(begin=0, end=9, autoPage=True):
         js = json.loads(gh_req.text)
         cnt = js['data'][rankname + '_count']
         for a in js['data'][rankname]:
-            item = Union(irank=a['iRank'], uuin=a['uUin'], unionname=a['unionName'], iscore=a['iScore'])
+            item = Union(irank=a['iRank'], uuin=a['uUin'], unionname=a['unionName'], iscore=a['iScore'], a_actid=actid)
             print("----------{}---------".format(item.unionname))
-            gh_cnt = crawl_acct(item.uuin)
+            gh_cnt = crawl_acct(actid, item.uuin)
             item.cnt = gh_cnt
             session.add(item)
             session.commit()
@@ -88,7 +129,7 @@ def crawl_union(begin=0, end=9, autoPage=True):
         time.sleep(sleep_interval)
 
 
-def crawl_acct(uUin):
+def crawl_acct(actid, uUin):
     """
     爬取工会内的账户信息
     :param uUin: 工会id
@@ -96,7 +137,7 @@ def crawl_acct(uUin):
     """
     begin = 0
     end = 9
-    rankname = "group_{}_rank_peract_339".format(uUin)
+    rankname = "group_{}_rank_peract_{}".format(uUin, actid)
     while True:
         gm_url = "https://c.y.qq.com/shop/fcgi-bin/fcg_album_rank?" \
                  "g_tk={}&uin={}&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1&rankname={}&begin={}&end={}&_={}" \
@@ -106,7 +147,7 @@ def crawl_acct(uUin):
         print(gm_url)
         cnt = js['data'][rankname + '_count']
         for a in js['data'][rankname]:
-            item = Acct(irank=a['iRank'], uuin=a['uUin'], strnick=a['strNick'], iscore=a['iScore'])
+            item = Acct(irank=a['iRank'], uuin=a['uUin'], strnick=a['strNick'], iscore=a['iScore'], a_actid=actid)
             item.u_uuin = uUin
             session.add(item)
             session.commit()
@@ -118,13 +159,29 @@ def crawl_acct(uUin):
     return cnt
 
 
+def delete(actid):
+    Act.query.filter(actid=actid).delete(synchronize_session=False)
+    Union.query.filter(a_actid=actid).delete(synchronize_session=False)
+    Acct.query.filter(a_actid=actid).delete(synchronize_session=False)
+    session.commit()
+
+
 if __name__ == '__main__':
     # 重建表
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
+    mid = '000etWPH2Sk6kG' # mmq
+
+    actid = crawl_sale_info(mid)
+    print(actid)
+
     # 爬取所有工会
-    crawl_union()
+    crawl_union(actid)
+
 
     # 只爬取某个工会
     # crawl_union(begin=12, end=12, autoPage=False)
+
+    # 删除
+    # delete(339)
--- query.sql
+++ query.sql
@@ -1,27 +1,52 @@
---截止12:19 1191088
+-- 专辑pk
+select
+a.actid,
+a.publictime as "发行时间",
+a.price as "单价(分)",
+a.sold_album_cnt as "总销量",
+a.soldamt as "总销售额(元)",
+a.album_name as "专辑",
+a.singername as "歌手",
+a.songcnt as "歌曲数",
+b.union_cnt as "工会数",
+b.iscore_cnt as "工会销量",
+a.price/100*b.iscore_cnt as "工会销售额(元)",
+100.0*a.price/100*b.iscore_cnt/a.soldamt as "占总销售额",
+b.acct_cnt as "工会人数",
+a.sold_album_cnt-b.iscore_cnt as "路人销量",
+a.price/100*(a.sold_album_cnt-b.iscore_cnt) as "路人销售额(元)",
+100.0*a.price/100*(a.sold_album_cnt-b.iscore_cnt)/a.soldamt as "占总销售额",
+c.cnt as "工会人数(可爬)"
+from
+(select * from qm_act) a
+join
+(select
+a_actid,
+count(1) as union_cnt,
+sum(cnt) as acct_cnt,
+sum(iscore) as iscore_cnt
+from qm_union group by a_actid) b
+on a.actid=b.a_actid
+join
+(select a_actid,count(1) as cnt from qm_acct  group by a_actid)c
+on a.actid=c.a_actid;
 
--- 工会数
-select count(1) as cnt from qm_union;
-
--- 工会账户数
-select count(1) as cnt from qm_acct;
-select sum(cnt) from qm_union;
-
--- 工会专辑数
-select sum(iscore) from qm_union;
-
-
--- 按账户数排名
+-- 工会大pk
 select a.*,b.* from
 (select u_uuin,count(1) as crwal_cnt from qm_acct group by u_uuin) a
 join
 (select * from qm_union) b
 on a.u_uuin=b.uuin
-order by a.crwal_cnt desc;
+order by b.iscore desc;
 
-select * from qm_acct where u_uuin='81211' order by iscore; --500后虽然取不到，但都是1了
+-- TODO:销量阶梯pk
+select a_actid,iscore,count(1) from qm_acct group by iscore,a_actid order by iscore,a_actid;
 
--- 按专辑数累计账户数
-select iscore,count(1) from qm_acct group by iscore order by iscore;
-
-
+-- 多担
+select uuin,
+count(distinct a_actid)as c,
+array_agg(a_actid)  ,
+array_agg(iscore)
+from qm_acct
+group by uuin
+having count(distinct a_actid) >1
